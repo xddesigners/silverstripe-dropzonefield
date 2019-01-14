96fc4cbfae1eaa129ddf6a47f951b909994069a8
Bram de Leeuw <bram@xd.nl>
2019-01-14 17:41:56 +0100

    remove chacnges from master

--- client/src/forms/DropzoneField.js
+++ client/src/forms/DropzoneField.js
@@ -9,24 +9,6 @@ dropzoneFields.forEach((dropzoneField) => {
   const schema = JSON.parse(input.attributes['data-schema'].value);
   const state = JSON.parse(input.attributes['data-state'].value);
   const dropzone = new Dropzone(field, config);
-
-  if (state.data.files.length) {
-    for (const i in state.data.files) {
-      const file = state.data.files[i];
-      const mockFile = { name: file.title, size: file.size };
-
-      // Call the default addedfile event handler
-      dropzone.emit("addedfile", mockFile);
-
-      // And optionally show the thumbnail of the file:
-      dropzone.emit("thumbnail", mockFile, file.thumbnail);
-
-      // Make sure that there is no progress bar, etc...
-      dropzone.emit("complete", mockFile);
-
-      dropzone.options.maxFiles = dropzone.options.maxFiles - 1;
-    }
-  }
   
   dropzone.on('success', (file, response) => {
     const input = document.createElement('input');
