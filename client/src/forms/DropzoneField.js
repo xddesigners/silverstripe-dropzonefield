790fe396ec4a865f2fe107d1ae63758cc2021327
Bram de Leeuw <bram@xd.nl>
2019-01-14 17:32:30 +0100

    test existing files

--- client/src/forms/DropzoneField.js
+++ client/src/forms/DropzoneField.js
@@ -7,11 +7,28 @@ dropzoneFields.forEach((dropzoneField) => {
   const input = dropzoneField.querySelector('input[type="file"]');
   const config = JSON.parse(input.attributes['dropzone-config'].value);
   const schema = JSON.parse(input.attributes['data-schema'].value);
+  const state = JSON.parse(input.attributes['data-state'].value);
   const dropzone = new Dropzone(field, config);
 
-  console.log(schema);
+  if (state.data.files.length) {
+    for (const i in state.data.files) {
+      const file = state.data.files[i];
+      const mockFile = { name: file.title, size: file.size };
+
+      // Call the default addedfile event handler
+      dropzone.emit("addedfile", mockFile);
+
+      // And optionally show the thumbnail of the file:
+      dropzone.emit("thumbnail", mockFile, file.thumbnail);
+
+      // Make sure that there is no progress bar, etc...
+      dropzone.emit("complete", mockFile);
+
+      dropzone.options.maxFiles = dropzone.options.maxFiles - 1;
+    }
+  }
+  
   dropzone.on('success', (file, response) => {
-    console.log(response);
     const input = document.createElement('input');
     input.type = 'hidden';
     input.name = `${schema.name}[Files][]`;
