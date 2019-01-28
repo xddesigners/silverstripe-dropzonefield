e369224349a432f6d0186a3747e3e2714c91aa94
Bram de Leeuw <bram@xd.nl>
2019-01-28 16:48:23 +0100

    disable dropzone from IE

--- client/src/forms/DropzoneField.js
+++ client/src/forms/DropzoneField.js
@@ -1,20 +1,26 @@
-import Dropzone from 'dropzone';
+import Dropzone from 'dropzone/dist/min/dropzone.min';
 Dropzone.autoDiscover = false;
 
+const IE = navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1);
 const dropzoneFields = document.querySelectorAll('.field.dropzone-field');
-for (let dropzoneField of dropzoneFields) {
-  const field = dropzoneField.querySelector('.dropzone');
-  const input = dropzoneField.querySelector('input[type="file"]');
-  const config = JSON.parse(input.attributes['dropzone-config'].value);
-  const schema = JSON.parse(input.attributes['data-schema'].value);
-  const state = JSON.parse(input.attributes['data-state'].value);
-  const dropzone = new Dropzone(field, config);
 
-  dropzone.on('success', (file, response) => {
-    const input = document.createElement('input');
-    input.type = 'hidden';
-    input.name = `${schema.name}[Files][]`;
-    input.value = response[0].id;
-    dropzoneField.appendChild(input);
-  });
+for (let dropzoneField of dropzoneFields) {
+  if (!IE) {
+    const field = dropzoneField.querySelector('.dropzone');
+    const input = dropzoneField.querySelector('input[type="file"]');
+    const config = JSON.parse(input.attributes['dropzone-config'].value);
+    const schema = JSON.parse(input.attributes['data-schema'].value);
+    const state = JSON.parse(input.attributes['data-state'].value);
+    const dropzone = new Dropzone(field, config);
+
+    dropzone.on('success', (file, response) => {
+      const input = document.createElement('input');
+      input.type = 'hidden';
+      input.name = `${schema.name}[Files][]`;
+      input.value = response[0].id;
+      dropzoneField.appendChild(input);
+    });
+  } else {
+    dropzoneField.classList.add('dropzone-field--ie');
+  }
 }
