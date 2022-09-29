35c2b63f09745d39c5b1faa48a72f58d0cfae57b
Bram de Leeuw <bram@xd.nl>
2022-09-29 15:57:58 +0200

    cleanup

--- client/src/js/ssdropzonefield.js
+++ client/src/js/ssdropzonefield.js
@@ -1,28 +1,25 @@
-// import Dropzone from 'dropzone/dist/min/dropzone.min';
-import { Dropzone } from "dropzone";
+import { Dropzone } from 'dropzone';
 Dropzone.autoDiscover = false;
 
 const IE = navigator.appName == 'Microsoft Internet Explorer' ||  !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1);
-const dropzoneFields = document.querySelectorAll('.field.dropzone-field');
 
-
-for (let dropzoneField of dropzoneFields) {
-
-  if (!IE ) {
+if (!IE) {
+  const dropzoneFields = document.querySelectorAll('.field.dropzone-field');
+  for (let dropzoneField of dropzoneFields) {
     dropzoneField.classList.add('dropzone-field--supported');
     const field = dropzoneField.querySelector('.dropzone');
     const input = dropzoneField.querySelector('input[type="file"]');
     const config = JSON.parse(input.attributes['dropzone-config'].value);
     const schema = JSON.parse(input.attributes['data-schema'].value);
     const state = JSON.parse(input.attributes['data-state'].value);
-
-    console.log('config', config);
-
+    
     const dropzone = new Dropzone(field, config);
-    dropzone.on("removedfile", file => {
-      
-      console.log('removedfile', file);
+    if (typeof state.data.files !== 'undefined' && state.data.files.length > 0) {
+      let file = state.data.files[0];
+      dropzone.displayExistingFile(file, file.thumbnail);
+    }
 
+    dropzone.on("removedfile", file => {
       if (config.hasOwnProperty('removeUrl')) {
         const req = new XMLHttpRequest();
         req.open('POST', config.removeUrl);
@@ -32,11 +29,6 @@ for (let dropzoneField of dropzoneFields) {
         req.send(JSON.stringify({fileId: file.serverId}));
       }
     });
-
-    if (typeof state.data.files !== 'undefined' && state.data.files.length > 0) {
-      let file = state.data.files[0];
-      dropzone.displayExistingFile(file, file.thumbnail);
-    }
 
     dropzone.on('success', (file, response) => {
       const input = document.createElement('input');
